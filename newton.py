28b44c66d46d1229bffe3f2c6a1157a4e85cfbfa
BMSales <bruno.m.sales03@gmail.com>
2025-09-17 16:39:47 -0300

    it now works

--- newton.py
+++ newton.py
@@ -2,13 +2,13 @@ import numpy as np
 import math
 
 def Function(m, x, e):
-    return m + x - e * np.sin(x)
+    return m - x + e * np.sin(x)
 
 def Derivative(x, e):
-    return 1 - e * np.cos(x)
+    return - 1 + e * np.cos(x)
 
 def Newton(m, eA, e):
-    while Function(m, eA, e) > 0.0000000001 or Function(m, eA, e) < -0.0000000001:
+    while Function(m, eA, e) > 0.0001 or Function(m, eA, e) < -0.0001:
         eA = eA - Function(m, eA, e)/Derivative(eA, e)
     else:
         return eA
@@ -16,8 +16,8 @@ def Newton(m, eA, e):
 def PositionOrbit(time, radSpeed, eccentricity, a, guess):
     m = radSpeed * time
     eA = Newton(m, guess, eccentricity)
-    trueAnomaly = math.sqrt((1 + eA)/(1 - eA)) * np.tan(eA/2)
-    trueAnomaly = np.arctan(trueAnomaly)*2
+    print(eA)
+    trueAnomaly = np.arctan(math.sqrt((1 + eccentricity)/(1 - eccentricity)) * np.tan(eA/2)) * 2
     r = a*(1 - eccentricity**2)/(1 + eccentricity * np.cos(trueAnomaly))
 
     return (r, trueAnomaly)
@@ -26,7 +26,8 @@ def PositionOrbit(time, radSpeed, eccentricity, a, guess):
 time = 600 # 10 minutos
 radSpeed = math.pi/3600
 a = 25
-eccentricity = 0.6
-guess = 20
+c = 15
+eccentricity = c/a
+guess = 2
 
 print(PositionOrbit(time, radSpeed, eccentricity, a, guess))
